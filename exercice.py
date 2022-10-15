9cf7125b76b536fdf5fe052665b1631f288a564f
Hundrick974 <harry.lawyen@gmail.com>
2022-10-14 20:54:03 -0400

    V1

--- exercice.py
+++ exercice.py
@@ -4,10 +4,16 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	liste = name.split('-')
+	prenom = liste[0].capitalize()
+	return "Bonjour " + prenom
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	phrase = "Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s."
+	liste_mots = []
+	for word_set in (animals, adjectives, fruits):
+		liste_mots += [word_set[random.randrange(0,len(word_set))]] 
+	return phrase % tuple(liste_mots) 
 
 def encrypt(text, shift):
 	return ""
